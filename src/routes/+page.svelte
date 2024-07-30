4dbc1754d2a8618a9c31aacb9bbca42c530845a3
David Eads <davideads@gmail.com>
2024-07-29 22:24:23 -0500

    Add tailwind.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,49 +11,47 @@
         console.log(out);
         selectedData = out || [];
     })
-  
-    // let hover = true;
-    // let hovered = null;
-
 </script>
 
-{#await data.app.stateData}
-<p>waiting...</p>
-{:then stateData}
+    {#await data.app.stateData}
+        <p>waiting...</p>
+    {:then stateData}
 
-<select bind:value={stateValue}>
-    {#each stateData.state_names as name}
-    <option value={name}>{name}</option>
-    {/each}
-</select>
+<div class="p-2 bg-gray-700">
+    <h1 class="font-bold text-xl text-white mb-1">State employment explorer</h1> 
+    
+    <p class="text-white mb-5">From the <a href="https://observablehq.com/@themarshallproject/census-labor-data-release">Annual Survey of Public Employment & Payroll</a>, 2000-2022.</p>
+    <div>
+        <select id="state-selector" bind:value={stateValue} class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            {#each stateData.state_names as name}
+            <option value={name}>{name}</option>
+            {/each}
+        </select>
+        <select id="measure-selector" bind:value={measureValue} class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            <option value="ft_employment">Full time employment</option>
+            <option value="ft_pay">Full time pay</option>
+        </select>
+    </div>
+</div>
+<div class="p-4">
+    <h1 class="text-4xl mt-10 uppercase">{stateValue}</h1>
+    <LineChart
+        height={600}
+        data={selectedData}
+        xKey="year"
+        yKey={measureValue}
+        zKey="gov_function"
+        labels="all"
+        selected={['education - higher education other']}
+        hover={true}
+        snapTicks={false}
+        animation={false}
+        padding={{ top: 0, bottom: 28, left: 35, right: 200 }}
+    />
+</div>
+    {:catch error}
 
-<select bind:value={measureValue}>
-    <option value="ft_employment">Full time employment</option>
-    <option value="ft_pay">Full time pay</option>
-</select>
-
-<h1>{stateValue}</h1>
-
-<p>chart</p>
-
-<LineChart
-    height={600}
-    data={selectedData}
-    xKey="year"
-    yKey={measureValue}
-    zKey="gov_function"
-    labels="hover"
-    color="gray"
-    selected={['education - higher education other']}
-    hover={true}
-    snapTicks={false}
-    animation={false}
-    padding={{ top: 0, bottom: 28, left: 35, right: 200 }}
-/>
-
-{:catch error}
-
-{/await}
+    {/await}
 
 <style>
   :global(*){
