44b8c2ffbdda89e1be2452382940ce333fbd3c18
David Eads <davideads@gmail.com>
2024-08-05 21:20:49 -0500

    Add info link back.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -54,7 +54,10 @@
                     {/each}
                 {/await}
             </select> 
-            <h1 class="ml-2">Employment Explorer</h1>
+            <h1 class="ml-2 flex-1">Employment Explorer</h1>
+            <div class="text-lg text-zinc-500 justify-self-end">
+                <a href="#about">ⓘ</a>
+            </div>
         </div> 
     </div>
     <div class="flex items-center bg-zinc-200 px-4 py-2 md:text-md">
@@ -68,86 +71,87 @@
             <input name="scale-selector" type="checkbox" bind:checked={sharedScale} /> Shared scale?
         </label>
     </div>
-        {#await data.app.stateData}
-            <div class="flex-1">
-                <div class="p-8">
-                    <p>Loading...</p>
-                </div>
+
+    {#await data.app.stateData}
+        <div class="flex-1">
+            <div class="mt-10 text-center">
+                <p>Loading...</p>
             </div>
-        {:then stateData}
-            <div class="flex border-b border-blue-900 bg-zinc-600 text-zinc-50 text-xs md:text-sm" let:index let:style {style}>
-                <div class="flex-none w-1/4 md:w-48 mr-5 py-2.5 px-4">Gov function</div>
+        </div>
+    {:then stateData}
+        <div class="flex border-b border-blue-900 bg-zinc-600 text-zinc-50 text-xs md:text-sm" let:index let:style {style}>
+            <div class="flex-none w-1/3 md:w-48 mr-5 py-2.5 px-2">Gov function</div>
+            <div class="flex-1 hidden md:flex">
+                <div class="flex-initial pl-5 w-80 py-2.5">FT employees</div>
+                <div class="flex-initial pl-5 w-80 py-2.5">Monthly pay per FT employee</div>
+                <div class="flex-initial pl-5 w-80 py-2.5">Monthly FT pay (total)</div>
+            </div>
+            <div class="flex-initial md:hidden py-2.5">{mobileChartField}</div>
+        </div>
+        <VirtualList width="100%" height={itemSize * itemCount} itemCount={govFunctions.length} {itemSize}>
+            <div slot="item" class="flex items-center border-b border-zinc-500" let:index let:style {style}>
+                <div class="flex-none w-1/3 md:w-48 uppercase text-xs md:text-sm px-2">
+                    {govFunctions[index]}
+                </div>
                 <div class="flex-1 hidden md:flex">
-                    <div class="flex-initial pl-5 w-80 py-2.5">FT employees</div>
-                    <div class="flex-initial pl-5 w-80 py-2.5">Monthly pay per FT employee</div>
-                    <div class="flex-initial pl-5 w-80 py-2.5">Monthly FT pay (total)</div>
-                </div>
-                <div class="flex-initial md:hidden py-2.5">{mobileChartField}</div>
-            </div>
-            <div class="w-full">
-                <VirtualList width="100%" height={itemSize * itemCount} itemCount={govFunctions.length} {itemSize}>
-                    <div slot="item" class="flex items-center border-b border-zinc-500" let:index let:style {style}>
-                        <div class="flex-none w-1/4 md:w-48 uppercase text-xs md:text-sm px-2">
-                            {govFunctions[index]}
-                        </div>
-                        <div class="flex-1 hidden md:flex">
-                            <div class="flex-initial pl-5 w-80">
-                                <MiniLineChart
-                                    height={itemSize}
-                                    data={selectedData.filter(d => d.gov_function === govFunctions[index])}
-                                    {xDomain}
-                                    yDomain = {[0, sharedScale ? maxFtEmployment : null]}
-                                    xKey="year"
-                                    yKey="ft_employment"
-                                    stroke="steelblue"
-                                />
-                            </div>
-                            <div class="flex-initial pl-5 w-80">
-                                <MiniLineChart
-                                    height={itemSize}
-                                    data={selectedData.filter(d => d.gov_function === govFunctions[index])}
-                                    {xDomain}
-                                    yDomain = {[0, sharedScale ? maxFtPayPerEmployee : null]}
-                                    xKey="year"
-                                    yKey="ft_pay_per_employee"
-                                    stroke="seagreen"
-                                />
-                            </div>
-                            <div class="flex-initial pl-5 w-80">
-                                <MiniLineChart
-                                    height={itemSize}
-                                    data={selectedData.filter(d => d.gov_function === govFunctions[index])}
-                                    {xDomain}
-                                    yDomain = {[0, sharedScale ? maxFtPay : null]}
-                                    xKey="year"
-                                    yKey="ft_pay"
-                                    stroke="mediumvioletred"
-                                />
-                            </div>
-                        </div>
-                        <div class="flex-1 md:hidden pl-5 pr-14">
-                            <MiniLineChart
-                                height={itemSize}
-                                data={selectedData.filter(d => d.gov_function === govFunctions[index])}
-                                {xDomain}
-                                yDomain = {[0, sharedScale ? mobileMax : null]}
-                                xKey="year"
-                                yKey={mobileChartField}
-                                stroke="steelblue"
-                            />
-                        </div>
+                    <div class="flex-initial pl-5 w-80">
+                        <MiniLineChart
+                            height={itemSize}
+                            data={selectedData.filter(d => d.gov_function === govFunctions[index])}
+                            {xDomain}
+                            yDomain = {[0, sharedScale ? maxFtEmployment : null]}
+                            xKey="year"
+                            yKey="ft_employment"
+                            stroke="steelblue"
+                        />
                     </div>
-                </VirtualList>
+                    <div class="flex-initial pl-5 w-80">
+                        <MiniLineChart
+                            height={itemSize}
+                            data={selectedData.filter(d => d.gov_function === govFunctions[index])}
+                            {xDomain}
+                            yDomain = {[0, sharedScale ? maxFtPayPerEmployee : null]}
+                            xKey="year"
+                            yKey="ft_pay_per_employee"
+                            stroke="seagreen"
+                        />
+                    </div>
+                    <div class="flex-initial pl-5 w-80">
+                        <MiniLineChart
+                            height={itemSize}
+                            data={selectedData.filter(d => d.gov_function === govFunctions[index])}
+                            {xDomain}
+                            yDomain = {[0, sharedScale ? maxFtPay : null]}
+                            xKey="year"
+                            yKey="ft_pay"
+                            stroke="mediumvioletred"
+                        />
+                    </div>
+                </div>
+                <div class="flex-1 md:hidden pl-5">
+                    <MiniLineChart
+                        height={itemSize}
+                        data={selectedData.filter(d => d.gov_function === govFunctions[index])}
+                        {xDomain}
+                        yDomain = {[0, sharedScale ? mobileMax : null]}
+                        xKey="year"
+                        yKey={mobileChartField}
+                        stroke="steelblue"
+                    />
+                </div>
             </div>
-        {/await}
+        </VirtualList>
+    {/await}
 </div>
 
 
-<div id="about" class="p-4 h-screen text-base md:text-lg mx-auto w-full md:max-w-2xl [&>p>a]:underline">
-    <p class="mb-8"><a href="#app">Back to explorer!</a></p>
-    <p class="mb-8 italic">By David Eads, first published July 30, 2024</p>
-    <p class="mb-4">These charts show the <a href="https://www.census.gov/programs-surveys/apes.html">Annual Survey of Public Employment & Payroll</a>, <a href="https://observablehq.com/@themarshallproject/census-labor-data-release">aggregated by state and government sector</a>, 2004-2022, based on the <a href="https://www.themarshallproject.org/2024/07/25/how-to-investigate-the-trend-of-declining-prison-staff-and-deteriorating-conditions-behind-bars">toolkit released by The Marshall Project</a>.</p>
-    <p class="mb-4">2004 was chosen as the starting point to avoid odd spikes in 2001-2002 that appear to be the result of a category that came and went.</p>
+<div id="about" class="h-screen">
+    <div class="p-4 text-base md:text-lg mx-auto w-full md:max-w-2xl [&>p>a]:underline">
+        <p class="mb-8"><a href="#app">Back to explorer!</a></p>
+        <p class="mb-8 italic">By David Eads, first published July 30, 2024</p>
+        <p class="mb-4">These charts show the <a href="https://www.census.gov/programs-surveys/apes.html">Annual Survey of Public Employment & Payroll</a>, <a href="https://observablehq.com/@themarshallproject/census-labor-data-release">aggregated by state and government sector</a>, 2004-2022, based on the <a href="https://www.themarshallproject.org/2024/07/25/how-to-investigate-the-trend-of-declining-prison-staff-and-deteriorating-conditions-behind-bars">toolkit released by The Marshall Project</a>.</p>
+        <p class="mb-4">2004 was chosen as the starting point to avoid odd spikes in 2001-2002 that appear to be the result of a category that came and went.</p>
+    </div>
 </div>
 
 <style>
